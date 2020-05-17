fa1701622e2a3825826e25f7053aec196399283f
chathurabuddi <chathu.buddi@gmail.com>
2020-05-17 08:35:43 +0530

    splash screen added

---
+++ app/src/main/java/lk/chathurabuddi/dashboard/corona/SplashActivity.java
@@ -0,0 +1,17 @@
+package lk.chathurabuddi.dashboard.corona;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
