d72655cd6ebab3934c759ee6b4922c0e2daf693d
chathurabuddi <chathu.buddi@gmail.com>
2020-05-18 17:26:33 +0530

    stat value suffix formats modified according to the google style

--- app/src/main/java/lk/chathurabuddi/dashboard/corona/util/AppUtil.java
+++ app/src/main/java/lk/chathurabuddi/dashboard/corona/util/AppUtil.java
@@ -4,16 +4,21 @@ import java.util.Locale;
 
 public class AppUtil {
     public static String format(long number) {
-        final long startValue = 1000000;
+        final long startValue = 1000;
         if (number < 0 ? -startValue < number : number < startValue){
             return String.valueOf(number);
         }
-        final String[] suffixes = { "m", "b", "t" };
+        final String[] suffixes = { "K", "M", "B", "T" };
         final int divider = 1000;
         double x = (double)number/startValue;
         for (String suffix : suffixes) {
             if (x < 0 ? -divider < x : x < divider) {
-                String formattedNumber = String.format(Locale.getDefault(),"%.2f", x);
+                String formattedNumber;
+                if ("K".equals(suffix)) {
+                    formattedNumber = String.valueOf((int)x);
+                } else {
+                    formattedNumber = String.format(Locale.getDefault(),"%.2f", x);
+                }
                 formattedNumber = formattedNumber.endsWith(".00")?formattedNumber.split(".")[0]:formattedNumber;
                 return  formattedNumber + suffix;
             }
