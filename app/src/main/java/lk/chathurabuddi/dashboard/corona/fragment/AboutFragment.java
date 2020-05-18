c99c35d2b1eab122c7bbe0f8fba26d76b77e3e13
chathurabuddi <chathu.buddi@gmail.com>
2020-05-18 17:30:18 +0530

    about fragment initial design

--- app/src/main/java/lk/chathurabuddi/dashboard/corona/fragment/AboutFragment.java
+++ app/src/main/java/lk/chathurabuddi/dashboard/corona/fragment/AboutFragment.java
@@ -1,12 +1,14 @@
 package lk.chathurabuddi.dashboard.corona.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import lk.chathurabuddi.dashboard.corona.R;
 
@@ -17,5 +19,26 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.inflateViews(view);
+    }
+
+    private void inflateViews(@NonNull View view) {
+        view.findViewById(R.id.fab).setOnClickListener(v -> {
+            FragmentManager fragmentManager = getFragmentManager();
+            if (fragmentManager != null) {
+                fragmentManager.beginTransaction().setCustomAnimations(
+                    R.anim.slide_in_right, R.anim.slide_out_left
+                ).replace(
+                    R.id.nav_host,
+                    ScreenFragment.DASHBOARD.getInstance(),
+                    ScreenFragment.DASHBOARD.name()
+                ).commit();
+            }
+        });
     }
 }
